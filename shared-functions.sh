bd7edaac5f870af8711eaf7345be77e74a028a4f
Rune Morling <ermo@serpentos.com>
2022-09-12 02:19:23 +0200

    Tweak output for 'git status' check on directory
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- shared-functions.sh
+++ shared-functions.sh
@@ -52,7 +52,7 @@ function checkGitStatusClean ()
     if [[ "$GIT_STATUS" == "" ]]; then
         return 0
     else
-        failMsg "\n  Git repo ${PWD} contains uncommitted changes.\n  '- Aborting!\n"
+        failMsg "\n  Git repo ${1} contains uncommitted changes.\n  '- Aborting!\n"
     fi
 }
 
