0d2ecca03d54cedece617c2aed1a9f23ea797da8
Rune Morling <ermo@serpentos.com>
2024-12-06 20:28:51 +0100

    shared: Add `moss-vendor` list or repos
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- shared-functions.sh
+++ shared-functions.sh
@@ -41,6 +41,7 @@ CORE_REPOS['libmoss']=main
 #CORE_REPOS['moss']=main
 #CORE_REPOS['moss-container']=main
 CORE_REPOS['moss-service']=main
+CORE_REPOS['moss-vendor']=main
 CORE_REPOS['summit']=main
 CORE_REPOS['vessel']=main
 
